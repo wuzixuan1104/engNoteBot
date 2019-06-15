4ae516031dd6bffcb96ccaec08bec251e15bc8c2
吳姿萱 <cherry51120@gmail.com>
2019-06-15 17:10:08 +0800

    liff card

--- app/lib/Menu.php
+++ app/lib/Menu.php
@@ -6,9 +6,6 @@ Load::lib('OALine/Line.php');
 
 class Menu {
   public static function existsCard($obj) {
-
-    $url = 'line://app/1585633080-5bbyNbWM?id=' . $obj->id . '&model=' . urlencode(get_class($obj));
-    
     return Msg::flex()->altText('學習卡片')->template( Msg\FlexTemplate::bubble([
       'body' => Msg\FlexBox::create([
         Msg\FlexText::create('Ennote 學英文')->setWeight('bold')->setColor('#47b0f5')->setSize('xs'),
@@ -19,7 +16,7 @@ class Menu {
 
         Msg\FlexBox::create([
           Msg\FlexButton::create('primary')->setHeight('sm')->setColor('#d6d6d6')->setAction(Msg\FlexAction::postback('刪除', ['cardDelete', get_class($obj), $obj->id], '已點擊「 刪除 」')),
-          Msg\FlexButton::create('primary')->setHeight('sm')->setColor('#e45a5a')->setAction(Msg\FlexAction::uri('編輯', $url)),
+          Msg\FlexButton::create('primary')->setHeight('sm')->setColor('#e45a5a')->setAction(Msg\FlexAction::uri('編輯', 'line://app/1585633080-5bbyNbWM?id=' . $obj->id . '&model=' . urlencode(get_class($obj)))),
         ])->setLayout('horizontal')->setMargin('lg')->setSpacing('lg'),
 
       ])->setLayout('vertical')])
