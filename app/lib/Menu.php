bfbecaef68da2660e05eef1754fcd711470bb8a9
吳姿萱 <cherry51120@gmail.com>
2019-06-15 14:29:07 +0800

    flex card

--- app/lib/Menu.php
+++ app/lib/Menu.php
@@ -11,17 +11,17 @@ class Menu {
       'body' => Msg\FlexBox::create([
         Msg\FlexText::create('Ennote 學英文')->setWeight('bold')->setColor('#47b0f5')->setSize('xs'),
 
-        Msg\FlexText::create('This is a book')->setWeight('bold')->setSize('md')->setWrap(true)->setMargin('lg'),
+        Msg\FlexText::create($obj->en)->setWeight('bold')->setSize('md')->setWrap(true)->setMargin('lg'),
 
-        Msg\FlexText::create('這是一本書')->setColor('#737373')->setSize('sm')->setWrap(true)->setMargin('lg'),
+        Msg\FlexText::create($obj->ch)->setColor('#737373')->setSize('sm')->setWrap(true)->setMargin('lg'),
         
-        Msg\FlexText::create('2019-04-10 11:23:11')->setColor('#cecece')->setSize('xxs')->setMargin('lg')->setAlign('end'),
+        Msg\FlexText::create($obj->updateAt)->setColor('#cecece')->setSize('xxs')->setMargin('lg')->setAlign('end'),
         
         Msg\FlexSeparator::create()->setMargin('sm'),
 
         Msg\FlexBox::create([
-          Msg\FlexButton::create('primary')->setHeight('sm')->setColor('#d6d6d6')->setAction(Msg\FlexAction::postback('刪除', ['cardDelete', 'w', 0], '已點擊「 刪除 」')),
-          Msg\FlexButton::create('primary')->setHeight('sm')->setColor('#e45a5a')->setAction(Msg\FlexAction::postback('編輯', ['cardEdit', 'w', 0], '已點擊「 編輯 」')),
+          Msg\FlexButton::create('primary')->setHeight('sm')->setColor('#d6d6d6')->setAction(Msg\FlexAction::postback('刪除', ['cardDelete', get_class($obj), $obj->id], '已點擊「 刪除 」')),
+          Msg\FlexButton::create('primary')->setHeight('sm')->setColor('#e45a5a')->setAction(Msg\FlexAction::postback('編輯', ['cardEdit', get_class($obj), $obj->id], '已點擊「 編輯 」')),
         ])->setLayout('horizontal')->setMargin('lg')->setSpacing('lg'),
 
       ])->setLayout('vertical')])
