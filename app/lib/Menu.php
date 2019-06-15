a5dcc6b7c5003043639f56e72d422cd7fd5513b5
吳姿萱 <cherry51120@gmail.com>
2019-06-15 14:40:34 +0800

    flex card

--- app/lib/Menu.php
+++ app/lib/Menu.php
@@ -6,7 +6,6 @@ Load::lib('OALine/Line.php');
 
 class Menu {
   public static function existsCard($obj) {
-    Log::info(get_class($obj));
     return Msg::flex()->altText('學習卡片')->template( Msg\FlexTemplate::bubble([
       'body' => Msg\FlexBox::create([
         Msg\FlexText::create('Ennote 學英文')->setWeight('bold')->setColor('#47b0f5')->setSize('xs'),
@@ -16,8 +15,8 @@ class Menu {
         Msg\FlexSeparator::create()->setMargin('sm'),
 
         Msg\FlexBox::create([
-          Msg\FlexButton::create('primary')->setHeight('sm')->setColor('#d6d6d6')->setAction(Msg\FlexAction::postback('刪除', ['cardDelete', 123, $obj->id], '已點擊「 刪除 」')),
-          Msg\FlexButton::create('primary')->setHeight('sm')->setColor('#e45a5a')->setAction(Msg\FlexAction::postback('編輯', ['cardEdit', 123, $obj->id], '已點擊「 編輯 」')),
+          Msg\FlexButton::create('primary')->setHeight('sm')->setColor('#d6d6d6')->setAction(Msg\FlexAction::postback('刪除', ['cardDelete', get_class($obj), $obj->id], '已點擊「 刪除 」')),
+          Msg\FlexButton::create('primary')->setHeight('sm')->setColor('#e45a5a')->setAction(Msg\FlexAction::postback('編輯', ['cardEdit', get_class($obj), $obj->id], '已點擊「 編輯 」')),
         ])->setLayout('horizontal')->setMargin('lg')->setSpacing('lg'),
 
       ])->setLayout('vertical')])
